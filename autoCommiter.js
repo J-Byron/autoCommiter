fd8ace8357feb222b88a7ce769f3793a07420283
j-byron <joshhbyron@gmail.com>
2024-11-26 01:01:34 -0500

    sc

--- autoCommiter.js
+++ autoCommiter.js
@@ -1,15 +1,23 @@
 const fs = require("fs");
 const simpleGit = require("simple-git");
+simpleGit().clean(simpleGit.CleanOptions.FORCE);
 
 // Path to the cloned repository
-const repoPath = "/workspace/autoCommiter"; // Cloud Run uses /workspace as the default working directory
-const filePath = `${repoPath}/README.md`;
+const filePath =
+  process.env.NODE_ENV === "production" ? "/workspace/README.md" : "README.md"; // Adjust path for Cloud Run or local
 const commitMessage = "Automated update to README.md";
 
-const git = simpleGit(repoPath).env({
-  GIT_ASKPASS: "echo",
-  GIT_PASSWORD: process.env.GIT_PAT,
-});
+const REPO = "autoCommiter";
+const USER = "j-byron";
+const PASS = "Amorfati2024";
+const gitHubUrl = `https://${USER}:${PASS}@github.com/${USER}/${REPO}`;
+
+const git = simpleGit();
+
+git
+  .addConfig("user.email", "joshhbyron@gmail.com")
+  .addConfig("user.name", "j-byron")
+  .addRemote("origin", gitHubUrl);
 
 // Random number of commits (0–5)
 const commitCount = Math.floor(Math.random() * 6); // Random between 0 and 5
@@ -31,11 +39,12 @@ if (commitCount === 0) {
       console.log(`Updated README.md at ${timestamp}`);
 
       // Step 2: Commit and push changes
-      await git.add(filePath);
-      await git.commit(commitMessage);
-      await git.push("origin", "main"); // Adjust branch name if needed
-
-      console.log("Changes committed and pushed successfully!");
+      await git
+        .add(filePath)
+        .commit(commitMessage)
+        .push(["-u", "origin", "main"], () =>
+          console.log("Changes committed and pushed successfully!")
+        );
     }
   } catch (error) {
     console.error("Failed to update README.md:", error);
